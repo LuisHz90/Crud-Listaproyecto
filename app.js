2333e2b3e88e25e2d0def630bb99c03343b56d06
Luis_Hernandez <luizhzgz@gmail.com>
2025-04-10 20:54:07 -0600

    3ro actualizao

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 console.log("prueba del script desde el archivo jss");
+
+localStorage.setItem("nombre", "Juan");
+localStorage.setItem("Auto", "Camionteta 4x4");
+
+const titulo = document.getElementById("ValorDelLocalStorage");
+titulo.innerHTML = localStorage.getItem("nombre");
+
+const botonBorrar = document.getElementById("botonBorrar");
+botonBorrar.addEventListener("click", () => {
+  localStorage.removeItem("nombre");
+  titulo.innerHTML = "Eliminado";
+});
+
+const botonBorrartodo = document.getElementById("botonBorrarTodo");
+botonBorrartodo.addEventListener("click", () => {
+  localStorage.clear();
+  titulo.innerHTML = "Eliminado Todo";
+});
